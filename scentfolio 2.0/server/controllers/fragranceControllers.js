17635823f0d00db99e90ac61934441bbb337e091
Minzo Kim <minzo.kim@gmail.com>
2023-07-27 15:44:56 -0400

    started controllers

---
+++ scentfolio 2.0/server/controllers/fragranceControllers.js
@@ -0,0 +1,8 @@
+const db = require('../models/fragranceModels');
+
+const fragranceController = {};
+
+fragranceController.addFragrance = (req, res, next) => {
+    console.log('made it to add fragrance controller');
+    // post request with
+};
